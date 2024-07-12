b7e99ba949afb8f98c2ce436e03238721b84908d
Fazliddin <kfazliddin191@gmail.com>
2024-07-12 18:28:36 +0500

    Navbar qo'shildi

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="">About</a></li>
+        <li><a href="">Contact</a></li>
+        <li><a href="">Sign</a></li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
